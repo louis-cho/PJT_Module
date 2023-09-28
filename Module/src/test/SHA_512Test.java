30df13994672146df6dfc10c634ad34f66f6340c
chohyunwoo <wizardchw@naver.com>
2023-09-28 10:46:00 +0900

    ..

--- Module/src/test/SHA_512Test.java
+++ Module/src/test/SHA_512Test.java
@@ -4,14 +4,14 @@ import security.SHA_512;
 
 public class SHA_512Test {
 
-	public static void main(String[] args) {
-		String plainText = "hello world";
-		System.out.println("origin text >> " + plainText);
-		for(int i=0; i<10; i++) {
-			String salt = SHA_512.getSalt();
-			String result = SHA_512.SHA512(plainText, salt);
-			
-			System.out.println("hash result >> " + result);
-		}
-	}
+    public static void main(String[] args) {
+        String plainText = "hello world";
+        System.out.println("origin text >> " + plainText);
+        for(int i=0; i<10; i++) {
+            String salt = SHA_512.getSalt();
+            String result = SHA_512.SHA512(plainText, salt);
+
+            System.out.println("hash result >> " + result);
+        }
+    }
 }
